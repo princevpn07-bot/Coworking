81b4e68d1fb0d5ae5207a98bff1503023405aed3
hijkd4637 <hijkd4637@gmail.com>
2026-05-28 15:46:57 +0800

    [feat] 修改付款頁面: 1.日期選單下拉 2.發票資訊呈現

--- coworking-admin/src/app/pages/frontend/payment-component/payment-component.ts
+++ coworking-admin/src/app/pages/frontend/payment-component/payment-component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './payment-component.html',
   styleUrl: './payment-component.css',
 })
-export class PaymentComponent {}
+export class PaymentComponent {
+  today: string = new Date().toISOString().split('T')[0];
+  startDate = '';
+  endDate = '';
+  invoiceType = 'personal';
+
+  onStartDateChange(event: Event): void {
+    this.startDate = (event.target as HTMLInputElement).value;
+  }
+
+  onEndDateChange(event: Event): void {
+    this.endDate = (event.target as HTMLInputElement).value;
+  }
+
+  onInvoiceTypeChange(event: Event): void {
+    this.invoiceType = (event.target as HTMLSelectElement).value;
+  }
+}
