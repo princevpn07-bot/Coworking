60769122f749d3e80cdf93beda89f9678a7f29fe
YU <maomiyoubai@gmail.com>
2026-06-15 16:22:17 +0800

    header選單新增 錨點滾動 、 favorite頁面連結

--- coworking-admin/src/app/app.config.ts
+++ coworking-admin/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter ,withInMemoryScrolling} from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
@@ -9,6 +9,13 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(
+      routes,
+    // 核心：啟用錨點滾動與跳頁流暢回復
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'})
+        )
   ]
 };
