7e8a654fab39b5ebc0bc9efcf040bfd03e49fd71
Tamanegi404 <PH19980107.L@gmail.com>
2026-06-24 21:47:00 +0800

    新增footer連結

--- coworking-admin/src/app/shared/frontend-footer/frontend-footer.ts
+++ coworking-admin/src/app/shared/frontend-footer/frontend-footer.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-frontend-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './frontend-footer.html',
   styleUrl: './frontend-footer.css',
 })
